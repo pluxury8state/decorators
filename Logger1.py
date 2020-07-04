58d743bd0f029253d68026e2a843a6e31c644eb8
Oleg Popov <ssdffgmlg@gmail.com>
2020-07-04 14:33:42 +0300

    first

---
+++ Logger1.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+
+def log_decor(old_function):
+    def new_function(*args, **kwargs):
+        obg = old_function()
+        info = dict()
+        info['record'] = datetime.utcnow()
+        info['args'] = args
+        info['kwargs'] = kwargs
+        info['resault'] = obg
+        info['name'] = old_function.__name__
+
+        with open('algo.txt', 'w', encoding = 'utf-8') as file:
+            for keys,values in info.items():
+                file.write(f'{keys} : {values}\n')
+
+    return new_function
+
+@log_decor
+def foo():
+    return 'hi'
+
+foo()
+
+
+
+
